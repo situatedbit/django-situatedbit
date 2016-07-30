877b23fa3b1cd708416d9b7d7d0c9d6f36476bfd
Matt Schaefer <matt@situatedbit.com>
2016-07-30 12:29:00 +0900

    initial commit with wagtail setup

---
+++ situatedbit/settings/dev.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '$0))g4a&c_miv)v9oxtqn4#g!6u6!6v69a!5zcuj)i15s$@%ai'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
