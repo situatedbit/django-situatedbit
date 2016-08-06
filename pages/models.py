69cbdbaabd08717f1fb644fb5d0ceaf83de21301
Matt Schaefer <matt@situatedbit.com>
2016-08-06 16:02:02 +0900

    initial commit of rich text page

---
+++ pages/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from wagtail.wagtailcore.models import Page
+
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailsearch import index
+
+class RichTextPage(Page):
+    body = RichTextField()
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
--- situatedbit/settings/base.py
+++ situatedbit/settings/base.py
@@ -27,6 +27,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 INSTALLED_APPS = [
     'home',
+    'pages',
     'search',
     'sitewide',
 
